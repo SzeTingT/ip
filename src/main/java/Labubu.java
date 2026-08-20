689f6b5f96b097d00969393e5e39a039c353ddaf
SzeTingT <0.tan.szeting@gmail.com>
2026-08-20 19:16:19 +0800

    Level-1

--- src/main/java/Labubu.java
+++ src/main/java/Labubu.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 /**
- * Starts the Labubu chatbot applOops, an error has occurredication.
+ * Starts the Labubu chatbot application.
  */
 public class Labubu {
     public static void main(String[] args) {
-        String banner =
+        String intro =
                   "____________________________________________________________\n"
                 + "██╗      █████╗ ██████╗ ██╗   ██╗██████╗ ██╗   ██╗\n"
                 + "██║     ██╔══██╗██╔══██╗██║   ██║██╔══██╗██║   ██║\n"
@@ -12,10 +14,19 @@ public class Labubu {
                 + "███████╗██║  ██║██████╔╝╚██████╔╝██████╔╝╚██████╔╝\n"
                 + "╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═════╝  ╚═════╝ \n"
                 + "Hello! I'm Labubu.\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println(banner);
+                + "What can I do for you?\n";
+        String exit =
+                "____________________________________________________________\n"
+              + "Bye. Hope to see you again soon!\n"
+              + "____________________________________________________________\n";
+        System.out.println(intro);
+        String userInput = "";
+        while (!userInput.equalsIgnoreCase("bye")) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("> ");
+            userInput = scanner.nextLine();
+            System.out.println(userInput);
+        }
+        System.out.println(exit);
     }
 }
