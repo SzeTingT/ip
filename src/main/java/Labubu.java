69b822340a70a4333400c41f484f9c218065c5b7
SzeTingT <0.tan.szeting@gmail.com>
2026-08-20 20:52:38 +0800

    Level-2

--- src/main/java/Labubu.java
+++ src/main/java/Labubu.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
+import java.util.*;
 
 /**
  * Starts the Labubu chatbot application.
  */
 public class Labubu {
     public static void main(String[] args) {
+        List<String> taskList = new ArrayList<>();
+
         String intro =
                   "____________________________________________________________\n"
                 + "██╗      █████╗ ██████╗ ██╗   ██╗██████╗ ██╗   ██╗\n"
@@ -19,13 +22,29 @@ public class Labubu {
                 "____________________________________________________________\n"
               + "Bye. Hope to see you again soon!\n"
               + "____________________________________________________________\n";
+
+        // Print introduction message
         System.out.println(intro);
-        String userInput = "";
-        while (!userInput.equalsIgnoreCase("bye")) {
+        String userInput = ""; // To store user input
+
+        while (true) { // Exit command
             Scanner scanner = new Scanner(System.in);
+
+            // Take in user input
             System.out.print("> ");
             userInput = scanner.nextLine();
-            System.out.println(userInput);
+
+            if (userInput.equalsIgnoreCase("bye")) { // Terminate; exit command
+                break;
+            }
+            else if (userInput.equalsIgnoreCase("list")) { // List task list
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println((i + 1) + ". " + taskList.get(i));
+                }
+            } else { // Else, add input to task list
+                taskList.add(userInput);
+                System.out.println("Added: " + userInput);
+            }
         }
         System.out.println(exit);
     }
